0dfb6bf692a18382f69d4aa384e42946c51720de
brendagold <petersbrenda47@gmail.com>
2025-11-10 17:50:34 -0500

    adds moto library

--- IaC/lambda/test_resumefunc.py
+++ IaC/lambda/test_resumefunc.py
@@ -1,11 +1,26 @@
 import unittest
+from moto import mock_aws
+import boto3
 from resumefunc import lambda_handler
 
+@mock_aws
 class TestLambda(unittest.TestCase):
+    def setUp(self):
+        # Create mock DynamoDB table
+        self.dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
+        self.table = self.dynamodb.create_table(
+            TableName="MyResumeViewCount",
+            KeySchema=[{"AttributeName": "id", "KeyType": "HASH"}],
+            AttributeDefinitions=[{"AttributeName": "id", "AttributeType": "S"}],
+            ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1}
+        )
+        self.table.put_item(Item={"id": "views", "count": 0})
+
     def test_get_method_returns_status_200(self):
         event = {"httpMethod": "GET"}
         response = lambda_handler(event, None)
         self.assertEqual(response["statusCode"], 200)
+        self.assertIn("views", response["body"])
 
     def test_invalid_method_returns_400(self):
         event = {"httpMethod": "POST"}
@@ -14,4 +29,3 @@ class TestLambda(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
